037c1ed26a3f68c10867ecc4d61557d08e97dc59
Freddymorato <Freddymorato@gmail.com>
2019-02-05 16:58:34 -0500

    creted comment actions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -86,3 +86,71 @@ export const deleteDestination = (destinationId, routerHistory) => {
     .catch(error => console.log(error))
   }
 };
+
+/***** COMMENT INDEX *****/
+
+const setComments = comments => {
+  return {
+    type: 'FETCH_COMMENTS',
+    comments
+  }
+};
+
+export const fetchComments = (destinationId) => {
+  return dispatch => {
+    dispatch({type: 'LOADING_COMMENTS'});
+      return fetch(`http://localhost:3000/destinations/${destinationId}/comments`)
+      .then(response => response.json())
+      .then(comments => dispatch(setComments(comments)))
+      .catch(error => console.log(error))
+  }
+};
+
+/***** COMMENT POST *****/
+
+const addComment = comment => {
+  return {
+    type: 'ADD_COMMENT',
+    comment
+  }
+};
+
+export const createComment = (comment) => {
+  return dispatch => {
+    return fetch(`http://localhost:3000/destinations/${comment.destination_id}/comments`, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ comment: comment })
+    })
+    .then(response => response.json())
+    .then(comment => { dispatch(addComment(comment)) })
+    .catch(error => { dispatch({ type: 'error' }) })
+  }
+};
+
+/***** LIKE DESTINATION *****/
+
+const addLikes = destination => {
+  return {
+    type: 'LIKE_DESTINATION',
+    destination
+  }
+};
+
+export const likeDestination = (destination) => {
+  const updatedDestination = Object.assign({...destination}, { likes: destination.likes + 1 })
+  return dispatch => {
+      return fetch(`http://localhost:3000/destinations/${destination.id}`, {
+        method: "PUT",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({destination: updatedDestination})
+      })
+      .then(response => response.json())
+      .then(destination => { dispatch(addLikes(destination)) })
+      .catch(error => console.log(error))
+  }
+};
