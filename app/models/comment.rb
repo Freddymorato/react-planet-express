37cbd68e2f341ec31ce684f350152fe9234b920b
Freddymorato <Freddymorato@gmail.com>
2019-02-05 15:46:24 -0500

    added validations to models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,4 @@
 class Comment < ApplicationRecord
+  belongs_to :destination
+  validates_presence_of :content, :commenter, :destination_id
 end
--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,2 +1,4 @@
 class Destination < ApplicationRecord
+  has_many :comments
+  validates_presence_of :name, :img_url, :astronomical_body, :view_on_maps, :radius, :description
 end
